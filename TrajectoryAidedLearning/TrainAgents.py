aa6ffc53c8766dc8f482f3e13946f15e85c65e85
Benjamin <benjaminevans316@gmail.com>
2022-11-24 08:43:33 +0200

    Training 6 m/s map results

--- TrajectoryAidedLearning/TrainAgents.py
+++ TrajectoryAidedLearning/TrainAgents.py
@@ -136,11 +136,11 @@ class TrainSimulation(TestSimulation):
 
 
 def main():
-    # run_file = "Cth_maps"
+    run_file = "Cth_maps"
     # run_file = "Cth_speeds"
     # run_file = "TAL_maps"
     # run_file = "TAL_speeds"
-    run_file = "Cth_speedMaps"
+    # run_file = "Cth_speedMaps"
     # run_file = "CthVsProgress"
 
     
