cfc18935801e855598d3628e4dd907505a4d3484
Benjamin <benjaminevans316@gmail.com>
2023-04-14 12:02:48 +0200

    PP bug that ensures repeatability

--- TrajectoryAidedLearning/Planners/PurePursuit.py
+++ TrajectoryAidedLearning/Planners/PurePursuit.py
@@ -316,7 +316,7 @@ class PurePursuit:
         # lookahead = 1.8
         # lookahead = 1.2
         lookahead = 1 + (self.max_speed/10) * state[3] /  self.max_speed
-        # lookahead = 0.8 + (self.max_speed/8) 
+        lookahead = 1 + 0.6 * state[3] / 8 # original....
         lookahead_point = self.trajectory.get_current_waypoint(position, lookahead)
 
         if state[3] < self.v_min_plan:
